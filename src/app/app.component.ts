ff329013f53c6ba278f4bf356ba580b08133a79d
unknown <ruli0016@gmail.com>
2022-02-11 10:45:06 +0100

    commit inicial

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title:string = 'Contador';
+  num:number = 0;
+}
+/* export class AppComponent {
+  title:string = 'Contador';
+  num:number = 0;
+} */
